97ab43d7b7e7b30db138a89e6495dd81982cfdc1
angelo Gonzalez <alpharogue71@gmail.com>
2025-10-25 00:51:19 +0200

    ajout des attributs de l'émulateur du controleur.

--- src/ControllerEmulator.java
+++ src/ControllerEmulator.java
@@ -1,7 +1,28 @@
-import java.util.Vector;
+import  Lib.ykVectorLib.*;
 
 public class ControllerEmulator {
 
+    private int leftTrigger = 0;
+    private int rightTrigger = 0;
+    private Vector2D rightStick = new Vector2D(0, 0);
+    private Vector2D leftStick = new Vector2D(0, 0);
+
+    private boolean buttonA = false;
+    private boolean buttonB = false;
+    private boolean buttonX = false;
+    private boolean buttonY = false;
+
+    private boolean buttonUp = false;
+    private boolean buttonDown = false;
+    private boolean buttonLeft = false;
+    private boolean buttonRight = false;
+
+    private boolean leftBumper = false;
+    private boolean rightBumper = false;
+
+    private boolean buttonStart = false;
+    private boolean buttonBack = false;
 
 
+    //lecture de Json contenant les inputs de test et d'émulation
 }
