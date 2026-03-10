a1afcf1b1ffcb6b5afad55c5abc416c05d7822b5
Yolkaal <alpharogue71@gmail.com>
2026-03-11 00:05:22 +0100

    engine base first working build

---
+++ src/main/java/ykSimEngine/eng/graph/Render.java
@@ -0,0 +1,20 @@
+package ykSimEngine.eng.graph;
+
+import ykSimEngine.eng.EngCtx;
+import ykSimEngine.eng.wnd.Window;
+import ykSimEngine.eng.scene.Scene;
+
+public class Render {
+
+    public Render(EngCtx engCtx) {
+        // To be implemented
+    }
+
+    public void cleanup() {
+        // To be implemented
+    }
+
+    public void render(EngCtx engCtx) {
+        // To be implemented
+    }
+}
---
+++ src/main/java/ykSimEngine/eng/scene/Scene.java
@@ -0,0 +1,9 @@
+package ykSimEngine.eng.scene;
+
+import ykSimEngine.eng.wnd.Window;
+
+public class Scene {
+
+    public Scene(Window window) {
+    }
+}
